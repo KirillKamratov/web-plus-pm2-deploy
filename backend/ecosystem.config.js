ad6578e5d26914eda4fbf006f25772fa61278e48
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-06-26 14:41:38 +0300

    added deploy script

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-backend',
+      script: './dist/app.js',
+    },
+  ],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/KirillKamratov/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: "../backend/.env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = "origin/master",
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-frontend",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/KirillKamratov/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      "post-deploy":
+        "cd frontend && npm install && npm run build && sudo scp -r build/* /var/www/build",
+    },
+  },
+};
