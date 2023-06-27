bea01190d337aec4f2b1cf9e5d38ce7971ef3e7e
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-06-27 21:39:02 +0300

    v2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,11 +12,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
