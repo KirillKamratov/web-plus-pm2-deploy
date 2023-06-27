9cbbcb7b1e16bc65c8ba8ac35103265882c53d63
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-06-27 21:50:45 +0300

    v2.2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
@@ -12,6 +13,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
+// Только для локальных тестов. Не используйте это в продакшене
+app.use(cors({
+  origin: [
+    'http://kamratov.students.nomoreparties.sbs',
+    'https://kamratov.students.nomoreparties.sbs',
+  ],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
